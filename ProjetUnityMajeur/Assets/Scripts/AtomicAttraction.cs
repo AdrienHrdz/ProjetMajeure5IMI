9da967ad8fce595dc746fa8e1e8238fc9556fd2f
AdrienHrdz <adrienhernandez16@gmail.com>
2023-01-09 11:10:32 +0100

    peerplay ep3 tmp

--- ProjetUnityMajeur/Assets/Scripts/AtomicAttraction.cs
+++ ProjetUnityMajeur/Assets/Scripts/AtomicAttraction.cs
@@ -12,18 +12,26 @@ public class AtomicAttraction : MonoBehaviour
     public float _spacingBetweenAttractPoints;
     [Range(0, 20)]
     public float _scaleAttractPoints;
+    GameObject[] _attractorArray, _atomArray;
+    [Range(1,64)]
+    public int _amoutOfAtomsPerPoint;
+    public Vector2 _atomScaleMinMax;
+    float[] _atomScaleSet;
+    public float _strenghtOfAttraction, _maxMagnitude, _randomPosDistance; 
+    public bool _useGravity;
 
     private void OnDrawGizmos()
     {
         for (int i = 0; i < _attractPoints.Length; i++)
         {
-            float evaluateStep = 1.0f / _attractPoints.Length;
-            Color color = _gradient.Evaluate(evaluateStep * i);
+            float evaluateStep = 0.125f;
+            Color color = _gradient.Evaluate(Mathf.Clamp(evaluateStep * _attractPoints[i], 0, 7));
             Gizmos.color = color;
 
-            Vector3 pos = new Vector3(transform.position.x + _spacingDirection.x * i * _spacingBetweenAttractPoints,
-                                      transform.position.y + _spacingDirection.y * i * _spacingBetweenAttractPoints,
-                                    transform.position.z + _spacingDirection.z * i * _spacingBetweenAttractPoints);
+            Vector3 pos = new Vector3(
+                transform.position.x + _spacingDirection.x * i * _spacingBetweenAttractPoints,
+                transform.position.y + _spacingDirection.y * i * _spacingBetweenAttractPoints,
+                transform.position.z + _spacingDirection.z * i * _spacingBetweenAttractPoints);
             Gizmos.DrawSphere(pos, _scaleAttractPoints);
         }
     }
@@ -31,7 +39,44 @@ public class AtomicAttraction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        _attractorArray = new GameObject[_attractPoints.Length];
+        _atomArray = new GameObject[_attractPoints.Length * _amoutOfAtomsPerPoint];
+        _atomScaleSet = new float[_attractPoints.Length * _amoutOfAtomsPerPoint];
+
+        int _countAtom = 0;
+        // instantiate points
+        for (int i = 0; i < _attractPoints.Length; i++)
+        {
+            GameObject _attractorInstance = (GameObject)Instantiate(_attractor);
+            _attractorArray[i] = _attractorInstance;
+
+            _attractorInstance.transform.position = new Vector3(
+                transform.position.x + _spacingDirection.x * i * _spacingBetweenAttractPoints,
+                transform.position.y + _spacingDirection.y * i * _spacingBetweenAttractPoints,
+                transform.position.z + _spacingDirection.z * i * _spacingBetweenAttractPoints);
+            _attractorInstance.transform.parent = this.transform;
+            _attractorInstance.transform.localScale = new Vector3(_scaleAttractPoints, _scaleAttractPoints, _scaleAttractPoints);
+
+            // instantiate atoms
+            for (int j = 0; j < _amoutOfAtomsPerPoint; j++)
+            {
+                Gameobject _atomInstance = (GameObject)Instantiate(_atom);
+                _atomArray[_countAtom] = _atomInstance;
+                _atomInstance.GetComponent<AttractTo>()._attractedTo = _attractorArray[i].transform;
+                _atomInstance.GetComponent<AttractTo>()._strenghtOfAttraction = _strenghtOfAttraction;
+                _atomInstance.GetComponent<AttractTo>()._maxMagnitude = _maxMagnitude;
+                _atomInstance.GetComponent<AttractTo>()._useGravity = _useGravity;
+                _atomInstance.transform.position = new Vector3(
+                    _attractorArray[i].transform.position.x + Random.Range(-_randomPosDistance, _randomPosDistance),
+                    _attractorArray[i].transform.position.y + Random.Range(-_randomPosDistance, _randomPosDistance),
+                    _attractorArray[i].transform.position.z + Random.Range(-_randomPosDistance, _randomPosDistance));
+                
+                float _randomScale = Random.Range(_atomScaleMinMax.x, _atomScaleMinMax.y);
+                _atomScaleSet[_countAtom] = _randomScale;
+                _atomInstance.transform.localScale = new Vector3(); 
+                _countAtom++;
+            }
+        }
     }
 
     // Update is called once per frame
