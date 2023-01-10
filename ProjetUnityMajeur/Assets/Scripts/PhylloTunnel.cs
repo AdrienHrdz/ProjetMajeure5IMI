7e05612d9e94255d0455f3ec342713168bf1365e
VincentMerrouche <vincent.merrouche@gmail.com>
2023-01-10 11:07:04 +0100

    audiopeer final

--- ProjetUnityMajeur/Assets/Scripts/PhylloTunnel.cs
+++ ProjetUnityMajeur/Assets/Scripts/PhylloTunnel.cs
@@ -9,10 +9,19 @@ public class PhylloTunnel : MonoBehaviour
     public int _scaleBand;
     public float _tunnelSpeed;
     public float _cameraDistance;
+    public bool _useBand;
     // Update is called once per frame
     void Update()
-    {
-        _tunnel.position = new Vector3(_tunnel.position.x, _tunnel.position.y, _tunnel.position.z + (AudioPeer._freqBand[_scaleBand] * _tunnelSpeed));
+    {   
+        if (_useBand)
+        {
+            _tunnel.position = new Vector3(_tunnel.position.x, _tunnel.position.y, _tunnel.position.z + (AudioPeer._audioBand[_scaleBand] * _tunnelSpeed));
+        }
+        else
+        {
+            _tunnel.position = new Vector3(_tunnel.position.x, _tunnel.position.y, _tunnel.position.z + (AudioPeer._Amplitude * _tunnelSpeed));  
+        }
+        
 
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, _tunnel.position.z + _cameraDistance);
     }
