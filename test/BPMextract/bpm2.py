747f5b135efcf927c762f988b8b22cc6be26d323
AdrienHrdz <adrienhernandez16@gmail.com>
2023-01-17 11:42:37 +0100

    test extract BPM

--- test/BPMextract/bpm2.py
+++ test/BPMextract/bpm2.py
@@ -3,7 +3,7 @@ import librosa
 import matplotlib.pyplot as plt
 import numpy as np
 
-filename = "purple.mp3"
+filename = "glue.mp3"
 audio, sr = librosa.load(filename, sr=None, mono=True)
 t = np.linspace(0, len(audio) / sr, len(audio))
 
@@ -26,7 +26,8 @@ while k1 < len(audio):
         k2 = len(audio)
     audio_crop = audio[k1:k2]
     # filter
-    apod = scipy.signal.windows.hann(k2 - k1)
+    # apod = scipy.signal.windows.hann(k2 - k1)
+    apod = scipy.signal.windows.blackmanharris(k2 - k1)
     audio_crop = audio_crop * apod
     filtered_audio = scipy.signal.lfilter(b, a, audio_crop)
     # fft
@@ -40,7 +41,7 @@ while k1 < len(audio):
     bpm_Hz = freq[index_bpm_Hz]
     bpm = bpm_Hz * 60
     bpmList.append(abs(bpm))
-    k1 = k2
+    k1 += sr
 
 
 
@@ -52,12 +53,25 @@ plt.ylabel('BPM')
 
 plt.subplot(212)
 bins = np.arange(90, 250, 1)
-plt.hist(bpmList, bins=bins)
+bpmHist, bin_edges = np.histogram(bpmList, bins=bins)
+# plt.plot(bpmHist[1][:-1], bpmHist[0], 'b.')
+plt.plot(bins[:-1], bpmHist)
 plt.xlabel('BPM')
 plt.ylabel('Occurences')
 
+id = np.argmax(bpmHist)
+# print(bin_edges[id], bin_edges[id+1])
+BPM = bin_edges[id+1]
+print(f'{filename} : BPM = {BPM:.2f}')
+
 plt.subplots_adjust(hspace=0.5)
 
+plt.subplot(211)
+plt.axhline(y=BPM, color='r', linestyle='--')
+plt.text(0, BPM+10, f'{BPM:.2f} BPM', color='r', fontsize=12)
 
+plt.suptitle(f'{filename}', fontsize=16)
+
+plt.savefig(f'.\img\{filename}_BPM.png')
 plt.show()
 
